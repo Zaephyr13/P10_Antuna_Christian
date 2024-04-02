3bc0b1382b07bf2da8f6e1556c7ef00201916dd7
Christian ANTUNA <chriscorps@hotmail.com>
2024-04-02 19:03:01 +0200

    Fix slider order from old to recent slide

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -10,9 +10,9 @@ const Slider = () => {
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     /* 
       Changement de l'opérateur logique pour trier dans le bon sens les images
-      De la plus récente à la plus ancienne (ordre décroissant)
+      De la plus ancienne à la plus récente (ordre croissant)
     */
-    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) > new Date(evtB.date) ? 1 : -1
   );
   const nextCard = () => {
     setTimeout(
